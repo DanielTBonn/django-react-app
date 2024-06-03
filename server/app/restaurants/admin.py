6e25a865be96172fcca851b87c61a12d9b026c8e
Daniel Bonn <danieltbonn@gmail.com>
2024-06-03 00:08:45 -0500

    finished creating api endpoints

--- server/app/restaurants/admin.py
+++ server/app/restaurants/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Restaurant
+from .models import Restaurant, Tag, Dish
+
+class DishInLine(admin.TabularInline):
+    model = Dish
+    extra = 3
+
+class RestaurantTagInLine(admin.TabularInline):
+    model = Tag.restaurants.through
+    extra = 3
 
 class RestaurantAdmin(admin.ModelAdmin):
     fieldsets = [ 
@@ -8,7 +16,28 @@ class RestaurantAdmin(admin.ModelAdmin):
         (None,              {'fields': ['latitude']}),
         (None,              {'fields': ['longitude']}),
     ]
+
+    inlines = [DishInLine, RestaurantTagInLine]
     list_display = ('name', 'address', 'latitude', 'longitude')
 
+class TagAdmin(admin.ModelAdmin):
+    fieldSets = [
+        (None, {'fields': ['name']}),
+    ]
+
+    inlines = [RestaurantTagInLine]
+    
+class DishAdmin(admin.ModelAdmin):
+    fieldSets = [
+        (None, {'fields': ['name']}),
+        (None, {'fields': ['description']}),
+        (None, {'fields': ['price']}),
+        (None, {'fields': ['restaurant']}),
+    ]
+
+    list_display = ('name', 'description', 'price', 'restaurant')
+
 admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Dish, DishAdmin)
 # Register your models here.

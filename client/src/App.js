fcd72cb605aee294290a4bfe7715dd5c942d4ccf
Daniel Bonn <danieltbonn@gmail.com>
2024-06-13 13:11:52 -0500

    added tailwindcss to react app

--- client/src/App.js
+++ client/src/App.js
@@ -32,7 +32,7 @@ function App() {
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
       <p>{ data && data.length ? data[0].fields.name : 'Loading...' }</p>
     </div>
   )
